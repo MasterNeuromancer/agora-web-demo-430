e4aa60baecbbe27c18909d1932050260d2ef828b
Neuromancer93 <itrejonumber1@gmail.com>
2022-04-30 17:02:55 -0700

    web rtc working on react app from agora sample

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Call from './Call.tsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Call />
+  </React.StrictMode>
+);
